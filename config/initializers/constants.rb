5136ed9278e9ab84d33fb683872f09f6cccc6852
Tim Escher <tim@eschers.com>
2015-01-07 10:09:36 -0800

    More static pages setup

---
+++ config/initializers/constants.rb
@@ -0,0 +1,9 @@
+# E-mail formatter
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+# Site Name
+SITE_TITLE = "Reach Out Lodi Volunteer Management"
+
+# Our Time Zone
+DEFAULT_TIME_ZONE = "Central Time (US & Canada)"
+
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = SITE_TITLE
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
 end
