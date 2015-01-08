5b12f2d787dd5194dc4e91633c1901f314ce6bcd
Tim Escher <tim@eschers.com>
2015-01-07 16:58:06 -0800

    Ruby version added

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,6 +3,8 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 # Site Name
 SITE_TITLE = "Reach Out Lodi Volunteer Management"
+ORG_TITLE = "Reach Out Lodi"
+ORG_SITE = "http://www.reachoutlogi.org"
 
 # Our Time Zone
 DEFAULT_TIME_ZONE = "Central Time (US & Canada)"
