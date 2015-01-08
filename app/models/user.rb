6a69fed164cd5f6b81d5a1d254284ac47ecc3d5d
Tim Escher <tim@eschers.com>
2015-01-08 04:46:45 -0800

    Initial user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+
+  before_save { self.email = email.downcase }
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
 end
