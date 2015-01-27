391372d0764797b4014f531fd0ca0674c2b24b4d
Tim Escher <tim@eschers.com>
2015-01-27 12:45:44 -0800

    Listing improvements

---
+++ app/assets/javascripts/utilities.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tescher on 1/27/15.
+ */
+
+jQuery(document).ready(function($) {
+    $(".row.clickable").click(function() {
+        window.document.location = $(this).attr("href");
+    });
+});
