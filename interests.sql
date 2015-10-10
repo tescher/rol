5c40229cde6ed3f2e8e029b7e950ec64683901ac
Tim Escher <tim@eschers.com>
2015-10-09 17:07:47 -0800

    sql dumps

--- interests.sql
+++ interests.sql
@@ -11,60 +11,91 @@ SET client_min_messages = warning;
 
 SET search_path = public, pg_catalog;
 
+ALTER TABLE ONLY public.donation_types DROP CONSTRAINT donation_types_pkey;
+ALTER TABLE public.donation_types ALTER COLUMN id DROP DEFAULT;
+DROP SEQUENCE public.donation_types_id_seq;
+DROP TABLE public.donation_types;
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
 --
--- Data for Name: interest_categories; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Name: donation_types; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
-COPY interest_categories (id, name, string, created_at, updated_at) FROM stdin;
-4	Office	\N	2015-02-06 17:49:32.23501	2015-02-06 17:49:32.23501
-6	Public Events	\N	2015-10-09 22:17:03.804856	2015-10-09 22:17:03.804856
-7	ReStore	\N	2015-10-09 22:17:18.116442	2015-10-09 22:17:18.116442
-5	Construction - Light	\N	2015-02-06 17:49:41.540354	2015-10-09 22:17:41.226113
-8	Construction - Skilled	\N	2015-10-09 22:17:53.58681	2015-10-09 22:17:53.58681
+CREATE TABLE donation_types (
+    id integer NOT NULL,
+    name character varying,
+    non_monetary boolean DEFAULT false NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    inactive boolean DEFAULT false NOT NULL
+);
+
+
+ALTER TABLE public.donation_types OWNER TO postgres;
+
+--
+-- Name: donation_types_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE donation_types_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.donation_types_id_seq OWNER TO postgres;
+
+--
+-- Name: donation_types_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE donation_types_id_seq OWNED BY donation_types.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY donation_types ALTER COLUMN id SET DEFAULT nextval('donation_types_id_seq'::regclass);
+
+
+--
+-- Data for Name: donation_types; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY donation_types (id, name, non_monetary, created_at, updated_at, inactive) FROM stdin;
+1	Cash	f	2015-10-07 00:59:11.991576	2015-10-07 00:59:11.991576	f
+2	Check	f	2015-10-07 00:59:23.89352	2015-10-07 00:59:23.89352	f
+3	ReStore	t	2015-10-07 00:59:36.803092	2015-10-08 21:06:57.719683	f
+4	PayPal	f	2015-10-08 21:07:08.71915	2015-10-08 21:07:08.71915	f
+5	Credit Card	f	2015-10-08 21:07:18.905221	2015-10-08 21:07:18.905221	f
+6	EFT	f	2015-10-08 21:07:28.132806	2015-10-08 21:07:28.132806	f
+8	ReStore DropOff	t	2015-10-08 21:07:56.527345	2015-10-08 21:07:56.527345	f
+9	ReStore PickUp	t	2015-10-08 21:08:10.720128	2015-10-08 21:08:10.720128	f
+7	Other Monetary	f	2015-10-08 21:07:41.351242	2015-10-08 21:52:33.418875	f
+10	Other Non-Monetary	t	2015-10-08 21:52:46.5182	2015-10-08 23:15:56.852698	f
 \.
 
 
 --
--- Name: interest_categories_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: donation_types_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('interest_categories_id_seq', 8, true);
-
-
---
--- Data for Name: interests; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY interests (id, name, interest_category_id, highlight, created_at, updated_at, inactive) FROM stdin;
-7	Data Entry	4	f	2015-02-06 17:50:05.737589	2015-02-06 17:50:05.737589	f
-9	Painting	5	f	2015-02-06 17:50:33.451562	2015-09-13 14:40:39.083624	f
-8	Reception	4	f	2015-02-06 17:50:21.576781	2015-10-09 22:18:33.500788	f
-11	Mailings	4	f	2015-10-09 22:18:45.528119	2015-10-09 22:18:45.528119	f
-12	Website	4	f	2015-10-09 22:18:58.179736	2015-10-09 22:18:58.179736	f
-10	Drywall	8	f	2015-02-06 17:50:45.973437	2015-10-09 22:19:15.967555	f
-14	Insulation	5	f	2015-10-09 22:19:50.62127	2015-10-09 22:19:50.62127	f
-15	Caulking	5	f	2015-10-09 22:20:03.179194	2015-10-09 22:20:03.179194	f
-16	Landscaping	5	f	2015-10-09 22:20:17.788077	2015-10-09 22:20:17.788077	f
-17	Provide Lunch	4	t	2015-10-09 22:20:30.154078	2015-10-09 22:20:30.154078	f
-18	Siding	8	f	2015-10-09 22:20:49.125777	2015-10-09 22:20:49.125777	f
-19	Trim - Finish Work	8	f	2015-10-09 22:21:02.486389	2015-10-09 22:21:02.486389	f
-20	Info Tables	6	f	2015-10-09 22:21:20.18592	2015-10-09 22:21:20.18592	f
-21	Fundraisers	6	f	2015-10-09 22:21:39.967496	2015-10-09 22:21:39.967496	f
-22	WomenBuild	6	f	2015-10-09 22:21:52.971884	2015-10-09 22:21:52.971884	f
-23	Shanty Town	6	f	2015-10-09 22:22:06.347121	2015-10-09 22:22:06.347121	f
-24	Customer Care	7	f	2015-10-09 22:22:20.811057	2015-10-09 22:22:20.811057	f
-25	Cashiering	7	f	2015-10-09 22:22:33.588927	2015-10-09 22:22:33.588927	f
-26	Stocking	7	f	2015-10-09 22:22:45.487274	2015-10-09 22:22:45.487274	f
-27	Deconstruction	7	t	2015-10-09 22:22:59.79123	2015-10-09 22:22:59.79123	f
-13	Cleaning - Sweeping	5	f	2015-10-09 22:19:37.836666	2015-10-09 22:40:22.033698	f
-\.
+SELECT pg_catalog.setval('donation_types_id_seq', 10, true);
 
 
 --
--- Name: interests_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: donation_types_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
-SELECT pg_catalog.setval('interests_id_seq', 27, true);
+ALTER TABLE ONLY donation_types
+    ADD CONSTRAINT donation_types_pkey PRIMARY KEY (id);
 
 
 --
