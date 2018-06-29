41b68d09d21f92b67589eb3bdc931c332ab3a398
Tim Escher <tim@eschers.com>
2018-06-29 09:42:42 -0500

    Debugging waiver add

--- app/helpers/waivers_helper.rb
+++ app/helpers/waivers_helper.rb
@@ -1,6 +1,7 @@
 module WaiversHelper
   def link_to_add_waiver_fields(volunteer_id, is_guardian)
     association = :waivers
+    puts "Is Guardian: #{is_guardian}"
     "add_fields_and_close(this, \'#{association}\', \'#{escape_javascript(add_waiver_fields(volunteer_id, is_guardian))}\', \'#{".add_waiver_fields"}\')"
   end
 
@@ -13,11 +14,12 @@ module WaiversHelper
       volunteer = Volunteer.find(volunteer_id)
       guardian = nil
     end
+    puts "Volunteer: #{volunteer.id}  Guardian: #{guardian ? guardian.id : "nil"}"
     output = ""
     association = :waivers
     form_builder = form_for(volunteer) do |builder|
       output = builder.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-        render("waivers/waiver_fields", :f => builder, :parent => volunteer, :guardian => guardian, :association => association)
+        render("waivers/waiver_fields", :f => builder, :parent => volunteer, :guardian => guardian, :association => association )
       end
     end
     output
