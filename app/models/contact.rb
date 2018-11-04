169c6cbe361f44bad28d94facc1105d8a2511dd7
Tim Escher <tim@eschers.com>
2018-11-04 14:51:26 -0800

    Contacts tests

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,5 +4,7 @@ class Contact < ActiveRecord::Base
   acts_as_paranoid
 
   validates_date :date_time, on_or_before: lambda { Date.today }, allow_blank: false
+  validates :contact_method_id, presence: true
+  validates :volunteer_id, presence: true
 
 end
