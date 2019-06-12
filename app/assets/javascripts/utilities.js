a21fef0f433fdcb98067e92aae8451d9631950ec
Tim Escher <tim@eschers.com>
2019-06-12 14:01:24 -0700

    Fix workday calculation

--- app/assets/javascripts/utilities.js
+++ app/assets/javascripts/utilities.js
@@ -370,11 +370,17 @@ function workday_hours_calc(node) {
     var parent_node = $(node).closest("tr");
     var start_string = $(parent_node).find("input[id*='start_time']").val();
     var end_string = $(parent_node).find("input[id*='end_time']").val();
+    $(parent_node).find("input[id*='end_time']").css("background-color","transparent");
     if (start_string && end_string) {
         var start = moment("2000-01-01 " + start_string, "YYYY-MM-DD h:mm A");
         var end = moment("2000-01-01 " + end_string, "YYYY-MM-DD h:mm A");
-        var diff = moment.duration(end.diff(start)).asHours().toFixed(1);
-        $(parent_node).find("input[id*='hours']").val(+diff);
+        if (end.isSameOrAfter(start)) {
+            var diff = moment.duration(end.diff(start)).asHours().toFixed(1);
+            $(parent_node).find("input[id*='hours']").val(+diff);
+        } else {
+            $(parent_node).find("input[id*='hours']").val("");
+            $(parent_node).find("input[id*='end_time']").css("background-color","lightpink");
+        }
     }
 }
 
